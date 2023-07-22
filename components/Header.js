0a3fce04562b13fdf75db304237506fd96d34c21
sameir13 <sameeraadil.x@gmail.com>
2023-07-22 02:31:11 -0700

    new nav

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,11 @@ import Link from "next/link";
 import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
 import { useSession, signOut } from "next-auth/react";
 import { Toast, Toaster, toast } from "react-hot-toast";
+import { useState } from "react";
 
 const Header = () => {
   const { data } = useSession();
+  const [select, setSelect] = useState(false);
 
   console.log(data);
 
@@ -14,76 +16,83 @@ const Header = () => {
       toast.success("Successfully Loggged Out");
     }
   };
+
+  const toggleMenu = () => {
+    setSelect(!select);
+  };
+
   return (
     <>
       <Toaster />
-      <div className="header">
-        <nav>
-          <div className="logo">
-            <h1>Bucket Blogs</h1>
-          </div>
 
-          {data?.user ? (
-            <div className="links">
-              <Link className="nav-links" href="/">
-                Home
-              </Link>
-              <Link className="nav-links" href="/about">
-                About
-              </Link>
-              <Link className="nav-links" href="/create">
-                Write
-              </Link>
-              <Link className="nav-links" href="/register">
-                Register
-              </Link>
-              <span
-                children
-                className="nav-links"
-                style={{
-                  cursor: "pointer",
-                  border: "1px solid #56B85D",
-                  backgroundColor: "#56B85D",
-                  padding: "3px 10px",
-                  color: "white",
-                  borderRadius: "5px",
-                }}
-                onClick={HandleSignOut}
-              >
-                Log out
-              </span>
-            </div>
-          ) : (
-            <>
-              <div className="nav-links-logout">
+      <div>
+        <div className={select ? "open" : null}>
+          <nav>
+            <img src="/images/blogger.png"  />
+          </nav>
+          <div class="overlay"></div>
+
+          <button class="burger" onClick={toggleMenu}>
+            <i
+              class="fa-solid fa-bars"
+              style={{ color: "Black", fontSize: "26px" }}
+            ></i>
+            <i
+              class="fa-solid fa-close"
+              style={{ color: "Black", fontSize: "26px" }}
+            ></i>
+          </button>
+
+          <aside>
+            {data?.user ? (
+              <div className="links">
                 <Link className="nav-links" href="/">
                   Home
                 </Link>
-                <Link className="nav-links" href={"/about"}>
+                <Link className="nav-links" href="/about">
                   About
                 </Link>
-                <Link
-                  href={"/login"}
+                <Link className="nav-links" href="/create">
+                  Write
+                </Link>
+                <Link className="nav-links" href="/register">
+                  Register
+                </Link>
+                <span
+                  children
                   className="nav-links"
                   style={{
                     cursor: "pointer",
                     border: "1px solid #56B85D",
+                    marginTop: "10px",
                     backgroundColor: "#56B85D",
                     padding: "3px 10px",
                     color: "white",
                     borderRadius: "5px",
                   }}
+                  onClick={HandleSignOut}
                 >
-                  Login
-                </Link>
+                  Log out
+                </span>
               </div>
-            </>
-          )}
+            ) : (
+              <>
+                <div className="nav-links-logout">
+                  <Link className="nav-links" href="/">
+                    Home
+                  </Link>
+                  <Link className="nav-links" href={"/about"}>
+                    About
+                  </Link>
 
-          <div className="HeaderHamMenu">
-            <i class="fa-solid fa-bars"></i>
-          </div>
-        </nav>
+                  <Link href={"/login"} className="nav-links-btn">
+                    Login
+                  </Link>
+                </div>
+              </>
+            )}
+          </aside>
+        </div>
       </div>
     </>
   );
