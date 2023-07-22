8f4b72dbc7bf1f7cf18abb66a2db7fc50740367f
sameir13 <sameeraadil.x@gmail.com>
2023-07-22 00:39:01 -0700

    added new form features

--- pages/login.js
+++ pages/login.js
@@ -1,37 +1,32 @@
-
 import { useState } from "react";
 import Header from "../components/Header";
 import Link from "next/link";
-import { signIn } from 'next-auth/react'
+import { signIn } from "next-auth/react";
 import Router from "next/router";
-
 
 const login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-
-  const router = Router
+  const router = Router;
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
     try {
-      const data = await signIn('credentials', {
+      const data = await signIn("credentials", {
         redirect: false,
         email,
-        password
+        password,
       });
-      console.log(data)
-      router.push("/")
-
+      console.log(data);
+      router.push("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
   return (
     <>
-     
       <div className="formOuter">
         <h1 className="trendingFont font-bold text-4xl p-6 text-center">
           Login here
@@ -40,6 +35,7 @@ const login = () => {
           <label htmlFor="email">Email: </label>
           <input
             required
+            autoComplete="off"
             type="text"
             id="email"
             onChange={(e) => setEmail(e.target.value)}
@@ -48,6 +44,7 @@ const login = () => {
           <label htmlFor="password">Password: </label>
           <input
             required
+            autoComplete="off"
             type="text"
             id="password"
             onChange={(e) => setPassword(e.target.value)}
@@ -55,9 +52,20 @@ const login = () => {
 
           <button type="submit">Login</button>
 
-          <div className="notAcc" style={{marginTop:"20px", textAlign:"center"}}>
+          <div
+            className="notAcc"
+            style={{ marginTop: "20px", textAlign: "center" }}
+          >
             <span>if you don't have an account!</span>
-            <Link className="link" href="/register" style={{marginLeft:"7px" , color:"blue" , borderBottom:"1px solid blue"}}>
+            <Link
+              className="link"
+              href="/register"
+              style={{
+                marginLeft: "7px",
+                color: "blue",
+                borderBottom: "1px solid blue",
+              }}
+            >
               Register
             </Link>
           </div>
